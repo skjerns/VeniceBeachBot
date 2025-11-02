e9a58ab84024bfce5e84bd4aa77358d21686fb6a
Simon Kern <14980558+skjerns@users.noreply.github.com>
2025-11-02 12:16:04 +0100

    add historical data switch

--- plotting.py
+++ plotting.py
@@ -49,158 +49,371 @@ def shade_leftover_hours(ax=None, **kwargs):
     return
 
 # @profile
+
 def make_plot(studio_id):
+
     csv_file = os.path.expanduser(f'./data/studio_{studio_id}.csv')
+
     png_file = os.path.expanduser(f'./data/studio_{studio_id}.png')
 
+
+
     if os.path.exists(png_file) and not debug:
+
         recent_png = ((time.time()-os.path.getmtime(png_file))<(10*60))
+
         if recent_png:
+
             return png_file
 
+
+
     studios = utils.load_studios()
+
     studio_name = studios.get(str(studio_id), 'Unknown Studio')
 
+
+
     now = datetime.datetime.now()
+
     de_holidays = holidays.Germany()
 
+
+
     df = pd.read_csv(csv_file, sep=';', names=['members', 'max'])
+
     df.members = df.members.convert_dtypes()
+
     df['max'] = df['max'].convert_dtypes()
+
     df['max'].loc[0] = 150
+
     n_max = max([int(x) for x in df['max'] if str(x).isnumeric()])
+
     df = df.drop('max', axis=1)
+
     df[df.members==-1] = np.nan
+
     df.index = pd.to_datetime(df.index, format='ISO8601').round('5min')
+
     df = df[~df.index.duplicated()]
+
     df = df.reindex(pd.date_range(start=df.index.min(),
+
                                   end=df.index.max(),
+
                                   freq='5min'))
+
     df = df.astype(float).interpolate(method='linear')
 
+
+
     if now.date() in de_holidays:
+
         holiday_name = de_holidays.get(now.date())
+
         holiday_dates = []
+
         for year in range(now.year - 1, now.year - 5, -1):
+
             try:
+
                 holiday_dates.extend([d for d, n in holidays.Germany(years=year).items() if n == holiday_name])
+
             except KeyError:
+
                 pass
+
         
+
         df_4week = df[df.index.date.isin(holiday_dates)]
+
     else:
+
         df_4week = df.loc[df.index > now-timedelta(days=7*4)]  # last four weeks
 
-    week_avg = df_4week.groupby((df_4week.index.dayofweek) * 24 +
-                                (df_4week.index.hour)).mean().rename_axis('HourOfWeek')
+
+
+    week_avg = df_4week.groupby((df_4week.index.dayofweek) * 24 +                                (df_4week.index.hour)).mean().rename_axis('HourOfWeek')
+
     week_avg = week_avg.reindex(np.arange(168), fill_value=np.nan)
+
     week_avg = np.array(week_avg.members).reshape([24, 7], order='F').T.astype(float)
 
 
+
+
+
     ######  plot last 4 weeks
+
     fig = plt.figure(figsize=[6, 8])
 
+
+
     # cmap = sns.color_palette("Spectral", as_cmap=True).reversed()
+
     cmap1 = plt.get_cmap('Spectral_r')
 
+
+
     colors = [(0.6196078431372549, 0.00392156862745098, 0.25882352941176473, 1.0),
+
               (0, 0, 0, 1)]
+
     cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", colors, N=256//4)
 
+
+
     colors = np.vstack([[cmap1(x) for x in range(256)],
+
                         [cmap2(x) for x in range(64)]])
+
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors, N=256+64)
 
+
+
     ax = plt.subplot(3, 1, 1)
+
     img = ax.imshow(week_avg[:,firsthour:], aspect='auto', cmap=cmap,
+
                vmin=n_max/10, vmax=n_max-n_max/10)
+
     highlight_now( color='r')
+
     ax.set_title(f'Durchschnitt letzte 4 Wochen')
+
     ax.set_xticks(np.arange(24-firsthour), np.arange(firsthour, 24))
+
     ax.set_yticks(np.arange(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
 
+
+
     ax.set_xlabel('Stunde')
+
     ax.set_ylabel('Wochentag')
+
     fig.colorbar(img)
+
+
 
     now = datetime.datetime.now()
 
+
+
     df_lastweek = df.loc[df.index > now-timedelta(days=7)] # this is the last week
+
     lastweek_avg = df_lastweek.groupby((df_lastweek.index.dayofweek) * 24 + (df_lastweek.index.hour)).mean().rename_axis('HourOfWeek')
+
     lastweek_avg = lastweek_avg.reindex(np.arange(168), fill_value=np.nan)
+
     lastweek_avg = np.array(lastweek_avg).reshape([-1,  7], order='F').T.astype(float)
+
+
 
     ######  plot last week
 
+
+
     plt.subplot(3, 1, 2)
+
     plt.imshow(lastweek_avg[:,firsthour:], aspect='auto', cmap=cmap,
+
                vmin=n_max/10, vmax=n_max-n_max/10)
+
     shade_leftover_hours()
+
     highlight_now(color='r')
 
+
+
     plt.title(f'diese Woche')
+
     plt.xticks(np.arange(24-firsthour), np.arange(firsthour, 24))
+
     plt.yticks(np.arange(7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
 
+
+
     plt.xlabel('Stunde')
+
     plt.ylabel('Wochentag')
+
     plt.colorbar()
 
+
+
     ######  plot today and forecast
+
     plt.subplot(3, 1, 3)
 
+
+
+    settings = utils.load_settings()
+
+    polling = settings.get('polling', True)
+
+
+
     prevh = 2
+
     nexth = 4
-    curr = df.loc[df.index > now+timedelta(hours=-prevh)]
-    curr.index = curr.index.round('5min')
-    curr = curr.astype('float').interpolate()
-    try:
-        ppl_now = int(curr.iloc[-1])
-    except:
-        ppl_now = 0
-    plt.plot(curr)
+
+    
+
+    if polling:
+
+        curr = df.loc[df.index > now+timedelta(hours=-prevh)]
+
+        curr.index = curr.index.round('5min')
+
+        curr = curr.astype('float').interpolate()
+
+        try:
+
+            ppl_now = int(curr.iloc[-1])
+
+        except:
+
+            ppl_now = 0
+
+        plt.plot(curr, label='heute')
+
+    else:
+
+        ppl_now = 'N/A'
+
+        plt.plot([], [], ' ', label='heute (unavailable)')
+
+        # Plot previous year instead of today
+
+        last_year_date = now - timedelta(days=365)
+
+        idx_last_year = ((df.index > last_year_date - timedelta(hours=prevh)) &
+
+                         (df.index < last_year_date + timedelta(hours=nexth)))
+
+        
+
+        prev_year_data = df[idx_last_year]['members']
+
+        if not prev_year_data.empty:
+
+            time_range_current = pd.date_range(start=now - timedelta(hours=prevh), end=now + timedelta(hours=nexth), periods=len(prev_year_data))
+
+            plt.plot(time_range_current, prev_year_data.values, label='previous year')
+
+
+
+
 
     weeks = []
+
     last_n = 7
+
     for w in range(-1, -last_n, -1):
+
         idx_week = ((df.index > now+timedelta(days=7*w, hours=-prevh)) &
+
                        (df.index < now+timedelta(days=7*w, hours=+nexth)))
+
         week = df[idx_week]['members']
+
         if len(week) > 0:
+
             weeks.append(week)
 
-    times = pd.date_range(now-timedelta(hours=+prevh), now+timedelta(hours=+nexth), freq='s')
-    times = times.round('5min').unique()
-    times = times[:len(weeks[-1])]
 
-    weeks = np.array(weeks)
-    b = np.zeros([len(weeks),len(max(weeks, key = lambda x: len(x)))])
 
-    plt.plot(times[:len(weeks[0])], np.nanmean(weeks, 0), linestyle='dashed', color='black', alpha=0.5)
+    if len(weeks) > 0:
 
-    weeksmin = np.nanmin(np.array(weeks), 0)
-    weeksmax = np.nanmax(np.array(weeks), 0)
-    plt.fill_between(times, weeksmin, weeksmax, color='gray', alpha=0.3)
+        times = pd.date_range(now-timedelta(hours=+prevh), now+timedelta(hours=+nexth), freq='s')
+
+        times = times.round('5min').unique()
+
+
+
+        min_len = min(len(w) for w in weeks)
+
+        weeks = [w[:min_len] for w in weeks]
+
+        times = times[:min_len]
+
+
+
+        weeks = np.array(weeks)
+
+        
+
+        plt.plot(times, np.nanmean(weeks, 0), linestyle='dashed', color='black', alpha=0.5, label='letzte Woche')
+
+
+
+        weeksmin = np.nanmin(np.array(weeks), 0)
+
+        weeksmax = np.nanmax(np.array(weeks), 0)
+
+        plt.fill_between(times, weeksmin, weeksmax, color='gray', alpha=0.3, label=f'letzte {last_n-1} Wochen')
+
+
 
     xformatter = mdates.DateFormatter('%H:%M')
+
     plt.gca().xaxis.set_major_formatter(xformatter)
-    weeksmaxmax = max(np.nan_to_num(weeksmax.max())*1.5, n_max)
-    plt.ylim([0, weeksmaxmax])
-    plt.legend(['heute', 'letzte Woche', f'letzte {last_n-1} Wochen'])
-    plt.title(f'Jetzt: {int(ppl_now)}/{n_max}')
-    plt.suptitle(f'{studio_name}: {int(ppl_now)}/{n_max}')
+
+    if len(weeks) > 0:
+
+        weeksmaxmax = max(np.nan_to_num(weeksmax.max())*1.5, n_max)
+
+        plt.ylim([0, weeksmaxmax])
+
+    
+
+    plt.legend()
+
+    
+
+    if polling:
+
+        title = f'Jetzt: {int(ppl_now)}/{n_max}'
+
+        suptitle = f'{studio_name}: {int(ppl_now)}/{n_max}'
+
+    else:
+
+        title = f'Jetzt: N/A/{n_max}'
+
+        suptitle = f'{studio_name}: N/A/{n_max}'
+
+
+
+    plt.title(title)
+
+    plt.suptitle(suptitle)
+
+
 
     plt.grid(which='major', alpha=0.5)
+
     plt.tight_layout()
+
     # i/o on the SD card is actually really fast
+
     # the slow process is the backend and converting to PNG/JPG
+
     # therefore we can easily save to disk and not to BytesIO w/o perf-loss
+
     # with stimer:
+
     plt.savefig(png_file)
+
     try:
+
        plt.close(fig)
+
     except:
+
        pass
+
     return png_file
 
 # import pandas as pd
